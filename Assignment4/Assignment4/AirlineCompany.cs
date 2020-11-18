59ba81f8e6d9ad4ed098ab708a97ad8d830fdccd
Anh Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-11-18 18:38:22 +0200

    Assignment 4 done

---
+++ Assignment4/Assignment4/AirlineCompany.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class AirlineCompany
+    {
+        private readonly string name;
+        List<Flight> collectionFlight;
+
+        public AirlineCompany(string name)
+        {
+            this.name = name;
+            this.collectionFlight = new List<Flight>();
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public int Count()
+        {
+            return this.collectionFlight.Count;
+        }
+
+        // Indexer
+        public Flight this[int index]
+        {
+            set
+            {
+                this.collectionFlight.Insert(index, value);
+            }
+            get
+            {
+                return this.collectionFlight[index];
+            }
+        }
+
+        public string FindAllFlight()
+        {
+            string res = "";
+            for (int i = 0; i < this.collectionFlight.Count(); i++)
+            {
+                res += this.collectionFlight[i].ToString();
+            }
+            return res;
+        }
+
+        public string SearchFlight(string flightId)
+        {
+            string res = "";
+            for (int i = 0; i < this.collectionFlight.Count(); i++)
+            {
+                res += this.collectionFlight[i].FindFlight(flightId);
+            }
+            return res;
+        }
+
+        public void GetFlightOriginAndDestination(Flight f)
+        {
+            Console.WriteLine("From: " + f.Origin + " to " + f.Destination + "\n");
+        }
+
+        // For delegate
+        public void GetFlightByPrice(processFlightDelate processDelegate, double price)
+        {
+            foreach (Flight f in this.collectionFlight)
+            {
+                if (f.Price > price)
+                    processDelegate.Invoke(f);
+            }
+        }
+
+        // For action
+        public void GetFlightByPriceAction(Action<Flight> actionFlight, double price)
+        {
+            foreach (Flight f in this.collectionFlight)
+            {
+                if (f.Price > price)
+                    actionFlight.Invoke(f);
+            }
+        }
+
+        public void GetFlightByPricePredicate(Predicate<Flight> predicateFlight)
+        {
+            int count = 0;
+            foreach (Flight f in this.collectionFlight)
+            {
+                if (predicateFlight.Invoke(f))
+                {
+                    Console.WriteLine("From: " + f.Origin + " to " + f.Destination + "\n");
+                    count++;
+                }
+                if(count == 1)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Assignment4/Assignment4/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    delegate void processFlightDelate(Flight f);
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string option;
+            string searchFlightId;
+            double searchPrice;
+
+            AirlineCompany FlightCollection = new AirlineCompany("Minh Hoang Airline");
+
+            //Action
+            Action<Flight> actionFlight = (Flight f) => FlightCollection.GetFlightOriginAndDestination(f);
+            //Predicate
+            Predicate<Flight> predicateFlight = (Flight f) => f.Price > 250;
+
+            FlightCollection[0] = new Flight("VN1", "Vaasa", "Ha Noi", new DateTime(2020, 10, 10, 8, 30, 52), 850.50);
+            FlightCollection[1] = new Flight("HCM1", "Ho Chi Minh", "Vaasa", new DateTime(2020, 9, 9, 8, 30, 52), 750.50);
+            FlightCollection[2] = new Flight("VA2", "Helsinki", "Vaasa", new DateTime(2020, 8, 8, 8, 30, 52), 100.65);
+            FlightCollection[3] = new Flight("PA3", "Vaasa", "Paris", new DateTime(2020, 7, 7, 8, 30, 52), 250.70);
+
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\n----------------List of options-----------------");
+                    Console.WriteLine("1: Get all flight information");
+                    Console.WriteLine("2: Search the flight id");
+                    Console.WriteLine("3: Search the flight price by using Delegate");
+                    Console.WriteLine("4: Search the flight price by using Action");
+                    Console.WriteLine("5: Find the first flight more than 250 euro by using Predicate");
+                    Console.WriteLine("exit: Exit the programming");
+                    Console.WriteLine("----------------End of options-----------------\n");
+
+                    Console.Write("Please choose your option: ");
+                    option = Console.ReadLine();
+                    switch (option)
+                    {
+                        case "1":
+                            Console.WriteLine("Here is all the flight information of the " + FlightCollection.getName() + ": \n");
+                            Console.WriteLine(FlightCollection.FindAllFlight());
+                            break;
+
+                        case "2":
+                            Console.Write("Please type flight id: ");
+                            searchFlightId = Console.ReadLine();
+                            Console.WriteLine(FlightCollection.SearchFlight(searchFlightId));
+                            break;
+
+                        case "3":
+                            Console.Write("Please type flight price: ");
+                            searchPrice = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("Here are all of flights less than " + searchPrice + ": \n");
+                            processFlightDelate pfd = new processFlightDelate(FlightCollection.GetFlightOriginAndDestination);
+                            FlightCollection.GetFlightByPrice(pfd, searchPrice);
+                            break;
+
+                        case "4":
+                            Console.Write("Please type flight price: ");
+                            searchPrice = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("Here are all of flights less than " + searchPrice + ": \n");
+                            FlightCollection.GetFlightByPriceAction(actionFlight, searchPrice);
+                            break;
+
+                        case "5":
+                            Console.Write("First Flight is greate than 250 euro: \n");
+                            FlightCollection.GetFlightByPricePredicate(predicateFlight);
+                            break;
+
+                        case "exit":
+                            Environment.Exit(0);
+                            break;
+
+                        default: break;
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Invalide data type!" + Environment.NewLine + e.Message);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
